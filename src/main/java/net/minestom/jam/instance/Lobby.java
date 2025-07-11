ea3ad016c9f84c029418f20b6a63ca61ee8f00a0
GoldenStack <git@goldenstack.net>
2025-07-11 17:54:32 -0500

    Prevent falling in void and block breaking in lobby

--- src/main/java/net/minestom/jam/instance/Lobby.java
+++ src/main/java/net/minestom/jam/instance/Lobby.java
@@ -4,6 +4,8 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.instance.AddEntityToInstanceEvent;
+import net.minestom.server.event.player.PlayerBlockBreakEvent;
+import net.minestom.server.event.player.PlayerMoveEvent;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.anvil.AnvilLoader;
 import org.jetbrains.annotations.NotNull;
@@ -34,6 +36,14 @@ public final class Lobby {
             if (!(event.getEntity() instanceof Player player)) return;
 
             onJoin(player);
+        }).addListener(PlayerMoveEvent.class, event -> {
+            final Player player = event.getPlayer();
+
+            if (player.getPosition().y() < 0) {
+                player.teleport(SPAWN_POINT);
+            }
+        }).addListener(PlayerBlockBreakEvent.class, event -> {
+            event.setCancelled(true);
         });
 
         return instance;
